b9ec8f99c0161008b9aba58a76d6c46718e60d0f
Sandy Davidson <sandy@sandysandy.net>
2018-06-08 00:10:31 +0100

    Accept lat and lng from search params in url

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ class App extends Component {
     zoom: 13,
   }
 
+  componentDidMount() {
+    let url = new URL(window.location.href); 
+    let searchParams = new URLSearchParams(url.search.slice(1));
+    const lats = [];
+    const lngs = [];
+    const combined = [];
+
+    for(let pair of searchParams.entries()) {
+      if (pair[0].startsWith('lat')) lats.push(pair[1])
+      if (pair[0].startsWith('lng')) lngs.push(pair[1])
+      console.log(pair[0]+ ', '+ pair[1]);
+    }
+
+    for (let i = 0; i < lats.length; i++) { 
+      combined.push({label: `Item ${i+1}`, lat: lats[i], lng: lngs[i]})
+    }
+
+    this.setState({ markers: combined})
+
+  }
+
   onSuggestionSelected = (selection) => {
     if(!this.state.markers.includes(selection)) {
       this.setState({ markers: [...this.state.markers, selection]})
